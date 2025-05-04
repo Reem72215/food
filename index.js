3ed1c59839d04454204e729f20236fc2875bf317
Reem72215 <ramromhj94@gmail.com>
2025-05-04 13:05:37 -0700

    first taskjava

---
+++ index.js
@@ -0,0 +1,25 @@
+var products = document.querySelectorAll('.primarybtn')
+var myBtn = document.getElementsByClassName('primarybtn')
+var myTotal = document.getElementById('total')
+var btnPrice = document.querySelector('.btn-price')
+var TotalPrice = 0
+var showPrice = document.getElementById('showPrice')
+
+
+
+
+products.forEach(function(item) {
+    item.addEventListener('click', function() {
+        TotalPrice += +(item.getAttribute('price'));
+        myTotal.innerHTML += item.getAttribute('foodName') +" "+item.getAttribute('price')+" "+"S.P"+"<br>";
+    })
+})
+
+function Draw(){
+    showPrice.innerHTML = (TotalPrice)+" "+"S.P";
+}
+
+myBtn.onclick = Draw;
+btnPrice.onclick = Draw;
+
+total.innerHTML = foodName + "<br>";
